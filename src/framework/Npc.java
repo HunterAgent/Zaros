ee74d167b4131a9e52dd17e5f280b4810c7cd21c
Lak3Scap3 <barghilnadav@gmail.com>
2022-10-14 18:12:48 +0300

    NPC framework

---
+++ src/framework/Npc.java
@@ -0,0 +1,32 @@
+package framework;
+
+import simple.hooks.wrappers.SimpleNpc;
+import simple.robot.api.ClientContext;
+
+import java.util.function.Predicate;
+
+public class Npc {
+    public static SimpleNpc getNearest(String ... name) {
+        return ClientContext.instance().npcs.populate().filter(name).nearest().next();
+    }
+
+    public static SimpleNpc getNearest(int ... id) {
+        return ClientContext.instance().npcs.populate().filter(id).nearest().next();
+    }
+
+    public static SimpleNpc getMonster(String ... name) {
+        return ClientContext.instance().npcs.populate().filter(name).filter(npc -> npc != null && !npc.isDead()).nearest().next();
+    }
+
+    public static SimpleNpc getMonster(int ... id) {
+        return ClientContext.instance().npcs.populate().filter(id).filter(npc -> npc != null && !npc.isDead()).nearest().next();
+    }
+
+    public static SimpleNpc getMonster(Predicate<SimpleNpc> npcFilter, int ... id) {
+        return ClientContext.instance().npcs.populate().filter(id).filter(npc -> npc != null && !npc.isDead()).filter(npcFilter).nearest().next();
+    }
+
+    public static boolean isValid(SimpleNpc npc) {
+        return npc != null && npc.visibleOnScreen();
+    }
+}
