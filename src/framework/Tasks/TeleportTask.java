20d643c585af9fed8c9121985ed1aefb84101c01
Lak3Scap3 <barghilnadav@gmail.com>
2022-10-15 11:18:40 +0300

    Base teleportation task

---
+++ src/framework/Tasks/TeleportTask.java
@@ -0,0 +1,36 @@
+package framework.Tasks;
+
+import framework.Player.Player;
+import framework.Teleportation.Location;
+import framework.Teleportation.Teleportation;
+import framework.World.Areas;
+import simple.hooks.scripts.task.Task;
+import simple.robot.api.ClientContext;
+import simple.robot.utils.WorldArea;
+
+public class TeleportTask extends Task {
+
+    private final Location loc;
+    private final WorldArea target;
+
+    public TeleportTask(ClientContext ctx, Location loc, WorldArea target) {
+        super(ctx);
+        this.loc = loc;
+        this.target = target;
+    }
+
+    @Override
+    public void run() {
+        Teleportation.teleport(this.loc);
+    }
+
+    @Override
+    public String status() {
+        return "Teleporting to " + loc.getName();
+    }
+
+    @Override
+    public boolean condition() {
+        return !Areas.containsIgnoreZ(this.target, Player.getLocation());
+    }
+}
