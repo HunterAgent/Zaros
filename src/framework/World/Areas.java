2340c6f6f64c145900340dcf23cec6c6371f27ef
Lak3Scap3 <barghilnadav@gmail.com>
2022-10-15 22:44:29 +0300

    draynor area

--- src/framework/World/Areas.java
+++ src/framework/World/Areas.java
@@ -14,6 +14,11 @@ public class Areas {
     public final static WorldArea BARROWS_FINAL_SARCO = new WorldArea(new WorldPoint(3547, 9700, 0), new WorldPoint(3558, 9690, 0));
     public static final WorldArea BANDOS_AREA = new WorldArea(new WorldPoint(2860, 5374, 2), new WorldPoint(2878, 5349, 2));
     public static final WorldArea ZULRAH_PRE_LOBBY = new WorldArea(new WorldPoint(2220, 3073, 0), new WorldPoint(2187, 3036, 0));
+    public static final WorldArea DRYNOR_VILLAGE = makeArea(3070, 3238, 3105, 3283);
+
+    public static WorldArea makeArea(int x, int y, int x2, int y2) {
+        return new WorldArea(new WorldPoint(x, y, 0), new WorldPoint(x2, y2, 0));
+    }
 
     public static WorldArea makeArea(int x, int y, int x2, int y2, int z) {
         return new WorldArea(new WorldPoint(x, y, z), new WorldPoint(x2, y2, z));
