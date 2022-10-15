dc9c077e71d2294b58b6df68b42c4a1defbde8e2
Lak3Scap3 <barghilnadav@gmail.com>
2022-10-15 21:38:02 +0300

    heal task

---
+++ src/framework/Tasks/RejuvenationBoxHealTask.java
@@ -0,0 +1,42 @@
+package framework.Tasks;
+
+import framework.Player.Player;
+import framework.Teleportation.Teleportation;
+import framework.World.Areas;
+import framework.World.WorldObject;
+import simple.hooks.scripts.task.Task;
+import simple.hooks.wrappers.SimpleObject;
+import simple.robot.api.ClientContext;
+
+public class RejuvenationBoxHealTask extends Task {
+    private final int threshold;
+    public RejuvenationBoxHealTask(ClientContext ctx, int threshold) {
+        super(ctx);
+        this.threshold = threshold;
+    }
+
+    @Override
+    public void run() {
+        if (!Areas.EDGEVILLE_AREA.containsPoint(Player.getLocation()))
+        {
+            Teleportation.home();
+        }
+
+        SimpleObject box = WorldObject.getNearest("Rejuvenation box");
+        if (WorldObject.isValid(box))
+        {
+            if (!box.click(0))
+                box.click("Heal");
+        }
+    }
+
+    @Override
+    public String status() {
+        return "Rejuvenating";
+    }
+
+    @Override
+    public boolean condition() {
+        return Player.getHealthPercent() < threshold;
+    }
+}
