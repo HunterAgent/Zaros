1913c6945827896a1457ef0a09b673349f6f9ae3
Lak3Scap3 <barghilnadav@gmail.com>
2022-10-15 14:37:41 +0300

    Fixed course travel task

--- src/Scripts/Agility/Tasks/CourseTravelTask.java
+++ src/Scripts/Agility/Tasks/CourseTravelTask.java
@@ -3,17 +3,22 @@ package Scripts.Agility.Tasks;
 import Scripts.Agility.Course;
 import framework.Tasks.TeleportTask;
 import lombok.Getter;
-import lombok.Setter;
 import simple.robot.api.ClientContext;
 
 public class CourseTravelTask extends TeleportTask {
 
     @Getter
-    @Setter
     private Course course;
 
     public CourseTravelTask(ClientContext ctx, Course course) {
         super(ctx, course.getLoc(), course.getArea());
+        this.course = course;
+    }
+
+    public void setCourse(Course course)
+    {
+        super.setLoc(course.getLoc());
+        super.setTarget(course.getArea());
         this.course = course;
     }
 }
