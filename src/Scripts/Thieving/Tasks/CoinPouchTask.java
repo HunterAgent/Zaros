2664fc91c1e8a00e24e7e0b53c88aa5a1e6cd53c
Lak3Scap3 <barghilnadav@gmail.com>
2022-10-16 14:08:30 +0300

    Coin pouch

---
+++ src/Scripts/Thieving/Tasks/CoinPouchTask.java
@@ -0,0 +1,31 @@
+package Scripts.Thieving.Tasks;
+
+import framework.Player.Inventory;
+import simple.hooks.scripts.task.Task;
+import simple.hooks.wrappers.SimpleItem;
+import simple.robot.api.ClientContext;
+
+public class CoinPouchTask extends Task {
+
+    private SimpleItem pouch;
+
+    public CoinPouchTask(ClientContext ctx) {
+        super(ctx);
+    }
+
+    @Override
+    public void run() {
+        pouch.click(0);
+    }
+
+    @Override
+    public String status() {
+        return "Opening coin pouches";
+    }
+
+    @Override
+    public boolean condition() {
+        pouch = Inventory.getItem("Coin Pouch");
+        return pouch != null && pouch.getQuantity() == 28;
+    }
+}
