494f9d96726c65ffc43b4b9253d842073076bfef
Lak3Scap3 <barghilnadav@gmail.com>
2022-10-16 10:50:19 +0300

    Prev teleport attempt

--- src/framework/Teleportation/Teleportation.java
+++ src/framework/Teleportation/Teleportation.java
@@ -1,13 +1,26 @@
 package framework.Teleportation;
 
 import framework.Player.Player;
+import framework.Widget;
 import framework.World.Areas;
+import simple.hooks.simplebot.Game;
 import simple.hooks.simplebot.teleporter.Teleporter;
+import simple.hooks.wrappers.SimpleWidget;
 import simple.robot.api.ClientContext;
 
 public class Teleportation {
     public static boolean teleport(Location loc)
     {
+//        Widget.openTab(Game.Tab.MAGIC);
+//        SimpleWidget homeTele = Widget.getWidget(218, 6);
+//        if (Widget.isValidWidget(homeTele) && homeTele.click(loc.getName()) && Player.isAnimating())
+//        {
+//            return true;
+//        }
+//        else {
+//            ClientContext.instance().mouse.clearMenu();
+//        }
+//
         Teleporter teleporter = new Teleporter(ClientContext.instance());
         if (!teleporter.opened())
             teleporter.open();
